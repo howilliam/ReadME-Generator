835023d0305005b97f5cd652f20b5ab0078ad934
howarceus <howarceus@gmail.com>
2024-02-02 19:00:10 +0000

    Added more details in the README options for readme generation and inquiries

--- index.js
+++ index.js
@@ -15,6 +15,42 @@ const questions = [
     name: "description",
     message: "Provide a short description of your project:",
   },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do you install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache", "GPL", "None"],
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "How can others contribute to your project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "How do you run tests on your project?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
 ];
 
 // function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,55 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
-
-  ## Description
-  ${data.description}
+  
+  // Table of Contents with links
+  const tableOfContents = `
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 `;
+
+  // README content
+  const readmeContent = `
+# ${data.title}
+
+${licenseBadgeUrl}
+
+## Description
+${data.description}
+
+${tableOfContents}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This application is covered under the ${
+    data.license
+  } license. [![License](${licenseBadgeUrl})](${getLicenseLink(data.license)})
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions about this project, please reach out to [${
+    data.github
+  }](https://github.com/${data.github}) or contact [${data.email}](mailto:${
+    data.email
+  }).
+`;
+
+  return readmeContent;
 }
 
 module.exports = generateMarkdown;
